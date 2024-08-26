4b0a35200715d96207e2da8ad842e0b8fbb576e9
Marrep13 <pon22601@uvg.edu.gt>
2024-08-26 17:50:05 -0600

    Create config.h

---
+++ config.h
@@ -0,0 +1,9 @@
+#define IO_USERNAME  "marrep"
+#define IO_KEY       "aio_ISWX96dJyuZdynjXC9uxjx7Y0qog"
+#define WIFI_SSID "Marre Ponce"
+#define WIFI_PASS "marreponce"
+
+
+#include "AdafruitIO_WiFi.h"
+
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
